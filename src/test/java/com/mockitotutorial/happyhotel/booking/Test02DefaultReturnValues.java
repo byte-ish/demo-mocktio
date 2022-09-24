2ed17dd8eab81c6411fb243b8c99b03dc335f4d2
Ish <ish.mishra@gmail.com>
2022-09-24 21:08:37 +0100

    Push changes
    Signed-off-by: Ish <ish.mishra@gmail.com>

---
+++ src/test/java/com/mockitotutorial/happyhotel/booking/Test02DefaultReturnValues.java
@@ -0,0 +1,42 @@
+package com.mockitotutorial.happyhotel.booking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+
+public class Test02DefaultReturnValues {
+
+    private BookingService bookingService;
+    private PaymentService paymentServiceMock;
+    private RoomService roomServiceMock;
+    private BookingDAO bookingDAOMock;
+    private MailSender mailSenderMock;
+
+    @BeforeEach
+    void setUp() {
+
+        this.paymentServiceMock = mock(PaymentService.class);
+        this.roomServiceMock = mock(RoomService.class);
+        this.bookingDAOMock = mock(BookingDAO.class);
+        this.mailSenderMock = mock(MailSender.class);
+        this.bookingService = new BookingService(paymentServiceMock, roomServiceMock, bookingDAOMock, mailSenderMock);
+
+        //Default values Returned
+        System.out.println("List returned: " + roomServiceMock.getAvailableRooms());
+        System.out.println("Object returned " + roomServiceMock.findAvailableRoomId(null));
+        System.out.println("Primitive returned " + roomServiceMock.getRoomCount());
+    }
+
+    @Test
+    void should_CountAvailablePlaces() {
+
+        //Given
+        int expected = 0;
+
+        //When
+
+        int actual = bookingService.getAvailablePlaceCount();
+        //Then
+    }
+}
